e29fb9196d0feed377aa396afb17b6d48443a92c
Stephen Kautt <stephengwk@gmail.com>
2025-02-28 23:34:08 -0600

    cooming

--- scenes/levels/area_2d.gd
+++ scenes/levels/area_2d.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
 @export var message: String
-@onready var speech_bubble = $SpeechBubble  # Assuming it's a child of the Area2D
+@onready var speech_bubble = get_tree().current_scene.find_child("SpeechBubble", true, false)
 
 func _on_body_entered(body):
-	if body.name == "Player":  # Ensure it reacts only to the player
-		speech_bubble.show_message("I don't want to go to class today!")
+	if body is CharacterBody2D:
+		print("Character entered! Trying to get SpeechBubble.")
+
+		if speech_bubble:
+			print("SpeechBubble found! Showing message.")
+			speech_bubble.show_message("I don't want to go to class today!")
+		else:
+			print("ERROR: SpeechBubble NOT found!")
