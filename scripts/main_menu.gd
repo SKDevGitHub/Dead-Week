f2dc4e447331691a05fd535070fbe3ca3933daaa
GharretWilcut <gharretwilcut@gmail.com>
2025-02-28 20:53:50 -0600

    finished menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,6 +7,5 @@ func _on_play_pressed() -> void:
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
-
-func _on_exit_pressed() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit()
