eb13ab09519c1c9064f07322faf576ce45ddb1df
GharretWilcut <gharretwilcut@gmail.com>
2025-02-28 20:42:08 -0600

    added some main menu action

---
+++ scripts/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/dorm.tscn")
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
