1190c2449d86f8f3037a3e34ad3b431be85e9124
bdsriver <bendachuan@gmail.com>
2025-03-01 16:55:52 -0600

    bullet damage

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
+@onready var area = $"."
 @export var direction:Vector2 = Vector2.ZERO
+@export var damage = 1
 
 func _ready():
-	pass
+	area.body_entered.connect(on_body_entered)
 	
 func _process(delta: float) -> void:
 	position += direction* delta * 500
+
+func on_body_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free() 
