4c9b62765706d9b0b2445979aadc9fe5e4e0cba2
GharretWilcut <gharretwilcut@gmail.com>
2025-03-01 21:25:31 -0600

    push some menu changes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,8 +4,6 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/dorm.tscn")
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
