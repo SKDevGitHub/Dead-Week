c04d90fe2600d5004a2abaa209709d008c6558fc
eliot <eliotkimmelcentillion@gmail.com>
2025-03-01 22:44:18 -0600

    progress on level 2

--- scripts/trapdoors.gd
+++ scripts/trapdoors.gd
@@ -3,11 +3,14 @@ extends Node2D
 @onready var timer: Timer = $Timer
 @onready var trapdoors = $Trapdoors
 
+var delay = 12
+
 func _on_timer_timeout() -> void:
-	var childs = trapdoors.get_children()
-	if !childs.is_empty():
-		childs[0].queue_free()
+	delay -= 1
+	if delay <= 0:
+		var childs = trapdoors.get_children()
+		if !childs.is_empty():
+			childs[0].queue_free()
 
 func _on_trigga_body_entered(body: Node2D) -> void:
 	timer.start()
-	print("Enter")
