ad412509ab91cab578ae555d5a6b36423e5246d1
bdsriver <bendachuan@gmail.com>
2025-03-01 23:27:33 -0600

    fixed cutscene sprites

--- scenes/levels/area_2d.gd
+++ scenes/levels/area_2d.gd
@@ -4,15 +4,19 @@ extends Area2D
 @onready var speech_bubble = $SpeechBubble
 
 var has_played: bool = false  # Track if the speech bubble has been triggered
+var player_body: CharacterBody2D
 
 func _on_body_entered(body):
 	if body is CharacterBody2D and not has_played:
+		player_body = body
 		print("Character entered! Showing message.")
 		if speech_bubble:
-			body.set_physics_process(false)  # Freeze player movement
+			player_body.in_cutscene = true  # Freeze player movement
+			player_body.velocity = Vector2.ZERO
 			speech_bubble.show_message(message, body, self)  # Pass self as reference
 		else:
 			print("ERROR: SpeechBubble NOT found!")
 
 func on_message_finished():
 	has_played = true  # Set this only after the message is done
+	player_body.in_cutscene = false
